f44a6aba13f4f1b7abcfb62c510083371cbfeee3
CXY0501 <chen.xy.2@foxmail.com>
2020-11-30 23:50:44 +0800

    20201130

---
+++ src/views/home/childcomps/featureView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="feature">
+    <a href="https://act.mogujie.com/zzxl67">
+      <img src="../../../assets/img/home/recommend_bg.jpg" alt="">
+    </a>
+  </div>
+</template>
+
+<script>
+export default{
+  name:'featureView',
+  data(){
+    return{}
+  },
+  methods:{
+  }
+}
+</script>
+
+<style scoped>
+  .feature img{
+    width: 100%
+  }
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -3,6 +3,27 @@
     <div id="navBar"><nav-bar><div slot="center">购物街</div></nav-bar></div>
     <home-swiper :banners="banners"></home-swiper>
     <home-recommend :recommends="recommends"/>
+    <feature-view/>
+    <li>content1</li>
+    <li>content2</li>
+    <li>content3</li>
+    <li>content4</li>
+    <li>content5</li>
+    <li>content6</li>
+    <li>content7</li>
+    <li>content8</li>
+    <li>content9</li>
+    <li>content10</li>
+    <li>content11</li>
+    <li>content12</li>
+    <li>content13</li>
+    <li>content14</li>
+    <li>content15</li>
+    <li>content16</li>
+    <li>content17</li>
+    <li>content18</li>
+    <li>content19</li>
+    <li>content20</li>
   </div>
 
 </template>
@@ -11,6 +32,7 @@
 import NavBar from '../../components/common/navbar/NavBar'
 import HomeSwiper from './childcomps/HomeSwiper.vue'
 import HomeRecommend from './childcomps/HomeRecommend'
+import featureView from './childcomps/featureView'
 import {getHomeMultidata} from '../../network/home'
 
 
@@ -19,7 +41,8 @@ export default{
   components:{
     NavBar,
     HomeSwiper,
-    HomeRecommend
+    HomeRecommend,
+    featureView
   },
   data(){
     return{
@@ -41,8 +64,16 @@ export default{
 </script>
 
 <style scoped>
+  #home{
+    padding-top: 44px;
+  }
   #navBar{
     background-color: var(--color-tint); 
-    color: #ffffff
+    color: #ffffff;
+    position:fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
   }
 </style>
