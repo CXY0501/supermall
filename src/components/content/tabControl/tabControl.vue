5f479df223f2ffc70919a0787e3e6267d31bff47
CXY0501 <chen.xy.2@foxmail.com>
2020-12-01 23:33:45 +0800

    20201201

---
+++ src/components/content/tabControl/tabControl.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="tab-control">
+    <div v-for="(item,index) in titles" :key="item" 
+    class="tabControl-item" :class="{active: index === currentIndex}" @click="itemClick(index)">
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default{
+  name:'tabControl',
+  data(){
+    return{
+      currentIndex:0
+    }
+  },
+  props:{
+    titles:{
+      type:Array,
+      default(){
+        return []
+      }
+    }
+  },
+  methods:{
+    itemClick(index){
+      this.currentIndex = index
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .tab-control{
+    display:flex;
+    font-size: 15px;
+    text-align: center;
+    height: 40px;
+    line-height: 40px;
+    background-color: #fff;
+  }
+  .tabControl-item{
+    flex:1;
+  }
+  .active{
+    color: var(--color-high-text);
+  }
+  .active span{
+    padding: 2px;
+    border-bottom: 3px solid var(--color-high-text);
+  }
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -4,35 +4,71 @@
     <home-swiper :banners="banners"></home-swiper>
     <home-recommend :recommends="recommends"/>
     <feature-view/>
-    <li>content1</li>
-    <li>content2</li>
-    <li>content3</li>
-    <li>content4</li>
-    <li>content5</li>
-    <li>content6</li>
-    <li>content7</li>
-    <li>content8</li>
-    <li>content9</li>
-    <li>content10</li>
-    <li>content11</li>
-    <li>content12</li>
-    <li>content13</li>
-    <li>content14</li>
-    <li>content15</li>
-    <li>content16</li>
-    <li>content17</li>
-    <li>content18</li>
-    <li>content19</li>
-    <li>content20</li>
+    <tab-control :titles="['流行','新款','精选']" class="tabControl"></tab-control>
+    <ul>
+      <li>1content</li>
+      <li>2content</li>
+      <li>3content</li>
+      <li>4content</li>
+      <li>5content</li>
+      <li>6content</li>
+      <li>7content</li>
+      <li>8content</li>
+      <li>9content</li>
+      <li>10content</li>
+      <li>11content</li>
+      <li>12content</li>
+      <li>13content</li>
+      <li>14content</li>
+      <li>15content</li>
+      <li>16content</li>
+      <li>17content</li>
+      <li>18content</li>
+      <li>19content</li>
+      <li>20content</li>
+      <li>21content</li>
+      <li>22content</li>
+      <li>23content</li>
+      <li>24content</li>
+      <li>25content</li>
+      <li>26content</li>
+      <li>27content</li>
+      <li>28content</li>
+      <li>29content</li>
+      <li>30content</li>
+      <li>31content</li>
+      <li>32content</li>
+      <li>33content</li>
+      <li>34content</li>
+      <li>35content</li>
+      <li>36content</li>
+      <li>37content</li>
+      <li>38content</li>
+      <li>39content</li>
+      <li>40content</li>
+      <li>41content</li>
+      <li>42content</li>
+      <li>43content</li>
+      <li>44content</li>
+      <li>45content</li>
+      <li>46content</li>
+      <li>47content</li>
+      <li>48content</li>
+      <li>49content</li>
+      <li>50content</li>
+    </ul>
   </div>
 
 </template>
 
 <script>
 import NavBar from '../../components/common/navbar/NavBar'
+import tabControl from '../../components/content/tabControl/tabControl'
+
 import HomeSwiper from './childcomps/HomeSwiper.vue'
 import HomeRecommend from './childcomps/HomeRecommend'
 import featureView from './childcomps/featureView'
+
 import {getHomeMultidata} from '../../network/home'
 
 
@@ -40,6 +76,7 @@ export default{
   name:'home',
   components:{
     NavBar,
+    tabControl,
     HomeSwiper,
     HomeRecommend,
     featureView
@@ -47,7 +84,12 @@ export default{
   data(){
     return{
       banners:[],
-      recommends:[]
+      recommends:[],
+      goods:{
+        'pop':{page:0, list:[]},
+        'new':{page:0, list:[]},
+        'sell':{page:0, list:[]},
+      }
     }
   },
   created(){
@@ -75,5 +117,9 @@ export default{
     right: 0;
     top: 0;
     z-index: 9;
+  }
+  .tabControl{
+    position: sticky;
+    top:44px
   }
 </style>
