01bad3633e37ff38e64af2db7f17d5054d438a93
CXY0501 <chen.xy.2@foxmail.com>
2020-12-12 17:01:04 +0800

    20201206-4

--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -1,31 +1,52 @@
 <template>
   <div id="detail">
-   <detail-nav-bar/>
-   <detail-swiper :top-images="topImages"/>
+   <detail-nav-bar class="detailNavBar"/>
+   <scroll class="content">
+    <detail-swiper :top-images="topImages"/>
+    <detail-base-info :goods="goods"/>
+    <detail-shop-info :shop="shop"/>
+    <detail-goods-info :detail-info="detailInfo"/>
+   </scroll>
   </div>
 </template>
 
 <script>
 import DetailNavBar from '../detail/detailChild/detailNavBar'
-import {getDetail} from '../../network/detail'
+import {getDetail,Goods,Shop} from '../../network/detail'
 import DetailSwiper from '../detail/detailChild/detailSwiper'
+import DetailBaseInfo from '../detail/detailChild/detailBaseInfo'
+import DetailShopInfo from '../detail/detailChild/detailShopInfo'
+import DetailGoodsInfo from '../detail/detailChild/detailGoodsInfo'
+
+import Scroll from '../../components/common/scroll/scroll'
 
 export default{
   name:'detail',
   components:{
     DetailNavBar,
-    DetailSwiper
+    DetailSwiper,
+    DetailBaseInfo,
+    DetailShopInfo,
+    Scroll,
+    DetailGoodsInfo
   },
   data(){
     return{
       iid:null,
-      topImages:[]
+      topImages:[],
+      goods:{},
+      shop:{},
+      detailInfo:{}
     }
   },
   created(){
     this.iid = this.$route.params.iid
     getDetail(this.iid).then(res=>{
-      this.topImages = res.result.itemInfo.topImages
+      const data=res.result
+      this.topImages = data.itemInfo.topImages
+      this.goods = new Goods(data.itemInfo,data.columns,data.shopInfo.services)
+      this.shop = new Shop (data.shopInfo)
+      this.detailInfo = data.detailInfo
     })
   },
   methods:{
@@ -34,4 +55,20 @@ export default{
 </script>
 
 <style scoped>
+#detail{
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+  height: 100vh;
+  
+}
+.detailNavBar{
+  position: relative;
+  z-index:9;
+  background-color: #fff;
+}
+.content{
+  height: calc(100% - 44px);
+  
+}
 </style>
