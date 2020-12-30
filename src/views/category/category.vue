98efeb856699d9672f72c227ee0dec7a41998dba
CXY0501 <chen.xy.2@foxmail.com>
2020-12-30 16:46:33 +0800

    2020123001

--- src/views/category/category.vue
+++ src/views/category/category.vue
@@ -1,17 +1,25 @@
 <template>
   <div>
     <category-nav-bar/>
-    <scroll class="MenuContent" 
-          ref="bscroll" 
-          :probe-type="3">
-      <category-menu/>
-    </scroll>  
+    <div class="categoryContent">
+      <scroll class="MenuContent" 
+            ref="bscroll" 
+            :probe-type="3">
+        <category-menu/>
+      </scroll>  
+      <scroll class="Miniwall" 
+            ref="bscroll" 
+            :probe-type="3">
+        <category-miniwall/>
+      </scroll>  
+    </div>
   </div>
 </template>
 
 <script>
 import CategoryNavBar from './categoryChild/categoryNavBar'
 import CategoryMenu from './categoryChild/categoryMenu'
+import CategoryMiniwall from './categoryChild/CategoryMiniwall'
 import scroll from '../../components/common/scroll/scroll'
 
 export default{
@@ -23,20 +31,27 @@ export default{
   components:{
     CategoryNavBar,
     CategoryMenu,
+    CategoryMiniwall,
     scroll
   },
   methods:{
-    test(){
-      console.log(this.categoryList)
-    }
   }
 }
 </script>
 
 <style scoped>
+.categoryContent{
+  display: flex;
+}
 .MenuContent{
   height: calc(100vh - 49px - 44px);
   z-index: 9;
+  overflow: hidden;
+  width: calc(100vw / 4);
+}
+.Miniwall{
+  width: calc(100vw *3 / 4);
+  height: calc(100vh - 44px - 49px);
   overflow: hidden;
 }
 </style>
