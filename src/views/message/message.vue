3fb67e242a7b07cef594c774a8acebfa1d7a196a
CXY0501 <chen.xy.2@foxmail.com>
2021-01-07 23:50:29 +0800

    2021010702

--- src/views/message/message.vue
+++ src/views/message/message.vue
@@ -2,13 +2,25 @@
   <div class="message">
     <nav-bar class="top">
       <div slot="left" class="title">
-          消息
+          <div>消息</div>
       </div>
       <div slot="center" class="topcenter">
             <img src="../../assets/img/message/contact.png" alt="">
       </div>
       <div slot="right" class="topright">
             <img src="../../assets/img/message/add.png" alt="">
+      </div>
+    </nav-bar>
+    <div class="noread">你有1300条未读消息</div>
+    <nav-bar class="middle">
+      <div slot="left" class="logistic">
+          <div>交易物流</div>
+      </div>
+      <div slot="center" class="notification">
+          <div>通知消息</div>
+      </div>
+      <div slot="right" class="interaction">
+          <div>互动消息</div>
       </div>
     </nav-bar>
   </div>
@@ -36,6 +48,8 @@ export default{
   color: #ffffff;
   font-size: 25px;
   font-weight: bold;
+  border: none;
+  box-shadow: none;
 }
 .center img{
   width: 38px;
@@ -48,5 +62,12 @@ export default{
   width: 26px;
   height: 26px;
   margin-top: 10px;
+}
+.noread{
+  background-color: var(--color-tint);
+  height: 150px;
+  color: #ffffff;
+  padding-left: 8px;
+  padding-top: 20px;
 }
 </style>
