31bf1362c575942272092936f24f43785b0244f0
CXY0501 <chen.xy.2@foxmail.com>
2021-01-09 21:43:31 +0800

    2020010901

--- src/views/message/messagechild/messageContent.vue
+++ src/views/message/messagechild/messageContent.vue
@@ -1,39 +1,15 @@
 <template>
   <div class="messageContent">
-      <nav-bar class="bar">
+      <nav-bar class="bar" v-for="(item,index) in messages" :key="index">
         <div slot="left" class="logo">
-          <img src="../../../assets/img/message/promotion.png" alt="">
+          <img :src="item.img" alt="">
         </div>
         <div slot="center" class="content">
-          <div class="main">活动优惠</div>
-          <div class="detail">亲，新款上了,来看看有没有您需要的？</div>
+          <div class="main">{{item.main}}</div>
+          <div class="detail">{{item.sub}}</div>
         </div>
         <div slot="right" class="reminder">
-          <div class="number">3</div>
-        </div>
-      </nav-bar>
-      <nav-bar class="bar">
-        <div slot="left" class="logo">
-          <img src="../../../assets/img/message/FIRE.png" alt="">
-        </div>
-        <div slot="center" class="content">
-          <div class="main">xx官方旗舰店</div>
-          <div class="detail">[专属客服为您服务]</div>
-        </div>
-        <div slot="right" class="reminder">
-          <div class="number">1</div>
-        </div>
-      </nav-bar>
-      <nav-bar class="bar">
-        <div slot="left" class="logo">
-          <img src="../../../assets/img/message/parcel.png" alt="">
-        </div>
-        <div slot="center" class="content">
-          <div class="main">xx物流</div>
-          <div class="detail">包裹已发出，查看送达时间</div>
-        </div>
-        <div slot="right" class="reminder">
-          <div class="number">1</div>
+          <div class="number">{{item.number}}</div>
         </div>
       </nav-bar>
   </div>
@@ -45,7 +21,82 @@ import NavBar from '../../../components/common/navbar/NavBar'
 export default{
   name:'MessageContent',
   data(){
-    return{}
+    return{
+      messages:[
+        {
+          img: require("../../../assets/img/message/promotion.png"),
+          main:'活动优惠',
+          sub:'亲，新款上了,来看看有没有您需要的？',
+          number:3
+        },
+        {
+          img: require('../../../assets/img/message/parcel.png'),
+          main:'xx物流',
+          sub:'包裹已发出，查看送达时间',
+          number:2
+        },
+        {
+          img: require('../../../assets/img/message/FIRE.png'),
+          main:'xx官方旗舰店',
+          sub:'[专属客服为您服务]',
+          number:20
+        },
+        {
+          img: require("../../../assets/img/message/promotion.png"),
+          main:'活动优惠',
+          sub:'亲，新款上了,来看看有没有您需要的？',
+          number:3
+        },
+        {
+          img: require('../../../assets/img/message/parcel.png'),
+          main:'xx物流',
+          sub:'包裹已发出，查看送达时间',
+          number:2
+        },
+        {
+          img: require('../../../assets/img/message/FIRE.png'),
+          main:'xx官方旗舰店',
+          sub:'[专属客服为您服务]',
+          number:5
+        },
+         {
+          img: require("../../../assets/img/message/promotion.png"),
+          main:'活动优惠',
+          sub:'亲，新款上了,来看看有没有您需要的？',
+          number:3
+        },
+        {
+          img: require('../../../assets/img/message/parcel.png'),
+          main:'xx物流',
+          sub:'包裹已发出，查看送达时间',
+          number:2
+        },
+        {
+          img: require('../../../assets/img/message/FIRE.png'),
+          main:'xx官方旗舰店',
+          sub:'[专属客服为您服务]',
+          number:20
+        },
+        {
+          img: require("../../../assets/img/message/promotion.png"),
+          main:'活动优惠',
+          sub:'亲，新款上了,来看看有没有您需要的？',
+          number:3
+        },
+        {
+          img: require('../../../assets/img/message/parcel.png'),
+          main:'xx物流',
+          sub:'包裹已发出，查看送达时间',
+          number:2
+        },
+        {
+          img: require('../../../assets/img/message/FIRE.png'),
+          main:'xx官方旗舰店',
+          sub:'[专属客服为您服务]',
+          number:5
+        }
+      ]
+    }
   },
   components:{
     NavBar
