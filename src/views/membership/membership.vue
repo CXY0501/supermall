db4a798f209e35e95cf6a20cf0ed3a0423c5d328
CXY0501 <chen.xy.2@foxmail.com>
2021-01-10 23:49:36 +0800

    2021011002

--- src/views/membership/membership.vue
+++ src/views/membership/membership.vue
@@ -1,14 +1,27 @@
 <template>
-  <div class="membership">
-    <h1>我是会员卡</h1>
+  <div>
+    <nav-bar class="membership">
+      <div slot="center">
+        您好，尊敬的会员
+      </div>
+    </nav-bar>
+    <div class="card">
+      <div class="cardcontent">
+        会员卡 - 普通会员
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import NavBar from '../../components/common/navbar/NavBar'
 export default{
   name:'Membership',
   data(){
     return{}
+  },
+  components:{
+    NavBar
   },
   methods:{
   }
@@ -16,4 +29,20 @@ export default{
 </script>
 
 <style scoped>
+.membership{
+  background-color: var(--color-tint);
+  color:#fff
+}
+.card{
+  margin-top: 100px;
+  margin-left: calc(100vw * 0.1);
+}
+.cardcontent{
+  height: 200px;
+  width: calc(100vw * 0.8);
+  background-color: var(--color-tint);
+  border-radius: 20px;
+  color:#fff;
+  text-align: center;
+}
 </style>
